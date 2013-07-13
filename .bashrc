53951f889d6d940cd0a2310f7234ee0f3c8dd628
seanchen <funningboy@gmail.com>
2013-07-13 17:06:33 +0800

    demo hw/sw regression

---
+++ .bashrc
@@ -0,0 +1,10 @@
+export PATH=/usr/local/bin/:/usr/bin:/opt/local/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/opt/local/lib:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=/usr/local/include:/usr/include:/opt/local/include:$C_INCLUDE_PATH
+
+# for mac port <port install> python packages
+export PYTHONPATH=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages:$PYTHONPATH
+
+# for normal <easy_install> python packages
+#export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
+
--- regression.py
+++ regression.py
@@ -1,8 +1,17 @@
+# HW/SW regression flow by python nose unittest
+# 1. using subprocess call third part system call
+# 2. using regular expression to parse the log
+# 3. register each testsuite to test Manager
+
 import sys
-import multiprocess
+import subprocess
 import unittest
 import nose
 import os
+import re
+
+reERROR = re.compile(r"ERROR", re.M)
+
 
 class TestHWSWAdd(unittest.TestCase):
     """ test HW SW add func """
@@ -10,12 +19,16 @@ class TestHWSWAdd(unittest.TestCase):
     def setUp(self):
         pass
 
-    def teardown(self):
+    def tearDown(self):
+        """ purge all """
+
         os.system('rm -rf *.vpi')
         os.system('rm -rf *.vvp')
         os.system('rm -rf *.o')
+        os.system('rm -rf *.log')
 
-    def buildup_vpi_db(vpi):
+
+    def buildup_vpi_db(self, vpi):
         """ build up vpi db called *.vpi ex: iverilog-vpi test.c """
 
         proc = subprocess.Popen("iverilog-vpi %s" %(vpi), \
@@ -23,7 +36,8 @@ class TestHWSWAdd(unittest.TestCase):
         id(proc)
         proc.wait()
 
-    def buildup_v_db(tsuite, vpp, vlist=[]):
+
+    def buildup_v_db(self, tsuite, vpp, vlist=[]):
         """ build up verilog db called *.vvp ex: iverilog -otest.vpp test.v """
 
         proc = subprocess.Popen("iverilog -s %s -o%s %s" %(tsuite, vpp, " ".join(vlist)), \
@@ -31,19 +45,43 @@ class TestHWSWAdd(unittest.TestCase):
         id(proc)
         proc.wait()
 
-    def run_v_vpi_db(m, vpp):
+
+    def run_v_vpi_db(self, m, vpp, log):
         """ run sim by verilog and vpi db """
 
-        proc = subprocess.Popen("vvp -M. -m%s %s" %(m, vpp), \
+        proc = subprocess.Popen("vvp -M. -m%s %s > %s" %(m, vpp, log), \
                 shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         id(proc)
         proc.wait()
 
-    def test_add_over_flow(self):
+
+    def analysis_log(self, log):
+        """ analysis the log is pass or fail """
+
+        f = open(log, 'r')
+        founds = reERROR.findall(f.read().replace('\n', ''))
+        f.close()
+
+        if founds:
+            return False
+        return True
+
+    def test_add_over_float(self):
         """ test add overfloat carry out, ..."""
+
+        self.buildup_vpi_db('./vpi/vpi_add.c')
+        self.buildup_v_db('test_over_float', 'vpi_add.vvp', ['dut.v', './test/test_dut.v'])
+        self.run_v_vpi_db('vpi_add', 'vpi_add.vvp', 'test_over_float.log')
+        self.assertTrue(self.analysis_log('test_over_float.log'))
+
 
     def test_add_normal(self):
         """ test add normal case """
+
+        self.buildup_vpi_db('./vpi/vpi_add.c')
+        self.buildup_v_db('test_add_normal', 'vpi_add.vvp', ['dut.v', './test/test_dut.v'])
+        self.run_v_vpi_db('vpi_add', 'vpi_add.vvp', 'test_add_normal.log')
+        self.assertTrue(self.analysis_log('test_add_normal.log'))
 
 
 if __name__ == '__main__':
